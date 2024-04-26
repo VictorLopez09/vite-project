457178cb0f2537bc33c7ed3b495f3b950ddb8337
VictorLopez09 <victor.lopez68@unach.mx>
2024-04-25 23:54:52 -0600

    Second-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { useState, useEffect } from 'react';
-import Select from 'react-select';
+import Select, { ValueType } from 'react-select';
 
 function App() {
-  const [opciones, setOpciones] = useState([]);
+  const [opcionesEstado, setOpcionesEstado] = useState([]);
+  const [opcionesCiudad, setOpcionesCiudad] = useState([]);
+  const [estadoSeleccionado, setEstadoSeleccionado] = useState<ValueType<OptionType>>(null);
 
   useEffect(() => {
-    fetchData();
+    fetchDataEstado();
   }, []);
 
-  const fetchData = async () => {
+  useEffect(() => {
+    if (estadoSeleccionado !== null) {
+      fetchDataCiudad(estadoSeleccionado?.value); // Aquí se pasa el valor directamente
+    }
+  }, [estadoSeleccionado]);
+
+  const fetchDataEstado = async () => {
     try {
       const response = await fetch('http://127.0.0.1:8000/api/State', {
         headers: {
@@ -17,7 +25,7 @@ function App() {
       });
   
       if (!response.ok) {
-        throw new Error('Error al obtener los datos');
+        throw new Error('Error al obtener los datos de estado');
       }
   
       const data = await response.json();
@@ -33,16 +41,61 @@ function App() {
         label: item.nombre_estado,
       }));
   
-      // Actualiza el estado con las opciones (usando useState)
-      setOpciones(options);
+      // Actualiza el estado con las opciones de estado
+      setOpcionesEstado(options);
     } catch (error) {
-      console.error('Error al obtener los datos:', error);
+      console.error('Error al obtener los datos de estado:', error);
     }
+  };
+
+  const fetchDataCiudad = async (claveEstado) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/City/${claveEstado}`, {
+        headers: {
+          'Accept': 'application/json',
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error('Error al obtener los datos de ciudad');
+      }
+  
+      const data = await response.json();
+  
+      // Mapea los datos a un formato compatible con react-select
+      interface Ciudad {
+        clave_ciudad: number;
+        nombre_ciudad: string;
+      }
+      
+      const options = data.map((item: Ciudad) => ({
+        value: item.clave_ciudad,
+        label: item.nombre_ciudad,
+      }));
+  
+      // Actualiza el estado con las opciones de ciudad
+      setOpcionesCiudad(options);
+    } catch (error) {
+      console.error('Error al obtener los datos de ciudad:', error);
+    }
+  };
+
+
+  //Mike este creo que es la falla o no se cual es la ffala mas bien
+  const handleEstadoChange = (selectedOption: ValueType<OptionType>) => {
+    setEstadoSeleccionado(selectedOption);
   };
 
   return (
     <div>
-      <Select options={opciones} />
+      <div>
+        <h2>Selecciona un estado:</h2>
+        <Select options={opcionesEstado} onChange={handleEstadoChange} />
+      </div>
+      <div>
+        <h2>Selecciona una ciudad:</h2>
+        <Select options={opcionesCiudad} />
+      </div>
     </div>
   );
 }
